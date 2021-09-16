e468fda25658ff3e1cfac99547abba250a079777
Yurij <nabor_2008@i.ua>
2021-09-16 11:49:59 +0300

    set view username

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -13,7 +13,7 @@
                 <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce <?= \Yii::$app->user->identity->username ?>
+                <a href="#" class="d-block">User: <?= \Yii::$app->user->identity->username ?>
                 </a>
             </div>
         </div>
